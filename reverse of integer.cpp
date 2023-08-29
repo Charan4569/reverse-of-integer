97c3662203017ab4adfafe0a2f556bfd2d3a2b78
NADIMPALLI SIVA CHARAN <143088679+Charan4569@users.noreply.github.com>
2023-08-30 00:07:57 +0530

    Add files via upload
    little steps on a long path

---
+++ reverse of integer.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int reverse(int x) {
+
+        long long int n,rev=0;
+
+        while(x!=0)
+
+        {
+            n=x%10;
+            x=x/10;
+            rev=rev*10+n;
+
+        }
+        cout<<"the reversed integer is:"<<endl;
+        if(rev<INT_MIN||rev>INT_MAX)
+        {
+            return 0;
+        }
+        else{
+            return rev;
+        }
+    }
+};
